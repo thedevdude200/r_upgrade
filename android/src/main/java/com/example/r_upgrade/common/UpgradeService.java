b07f25d7e12e13f7c5c57ca3973ddfd6e261b381
thedevdude200 <144073555+thedevdude200@users.noreply.github.com>
2025-01-17 11:55:22 +0800

    Update UpgradeService.java

--- android/src/main/java/com/example/r_upgrade/common/UpgradeService.java
+++ android/src/main/java/com/example/r_upgrade/common/UpgradeService.java
@@ -133,7 +133,13 @@ public class UpgradeService extends Service {
         } else {
             filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
         }
-        registerReceiver(actionReceiver, filter);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            registerReceiver(actionReceiver, filter, Context.RECEIVER_NOT_EXPORTED);
+        } else {
+            registerReceiver(actionReceiver, filter);
+        }
 
     }
 
